70892afd4bc022960f7ba2adf4db138cf65a99de
iosbd <io232001@gmail.com>
2023-02-27 11:22:46 +0100

    Commit

--- src/it/develhope/encapsulation2/Person.java
+++ src/it/develhope/encapsulation2/Person.java
@@ -1,13 +1,25 @@
 package it.develhope.encapsulation2;
-
-import lombok.Data;
-import lombok.ToString;
-
-@ToString
-public class Person
-{
-    public String name;
-    public String surname;
-    public double height;
-    public int age;
+public class Person {
+    private String name;
+    private String surname;
+    private double height;
+    private int age;
+    public Person(String name, String surname, double height, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.height = height;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public int getAge() {
+        return age;
+    }
 }
--- src/it/develhope/encapsulation2/main.java
+++ src/it/develhope/encapsulation2/main.java
@@ -1,14 +1,24 @@
 package it.develhope.encapsulation2;
-
+import java.text.DecimalFormat;
+import java.util.Scanner;
 public class main
 {
     public static void main (String args[])
     {
-        Person p1 = new Person();
-        p1.age=20;
-        p1.height=1.88;
-        p1.name="Mario";
-        p1.surname="Rossi";
-        System.out.println(p1.toString());
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter your surname: ");
+        String surname = scanner.nextLine();
+        System.out.print("Enter your height: ");
+        double height = scanner.nextDouble();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        Person person = new Person(name, surname, height, age);
+        DecimalFormat df = new DecimalFormat("0.00");
+        System.out.println("Name: " + person.getName());
+        System.out.println("Surname: " + person.getSurname());
+        System.out.println("Height: " + df.format(person.getHeight()) + " m");
+        System.out.println("Age: " + person.getAge());
     }
 }
