c8f7e124a66e4e61ca3a5605059d79f47cff00d6
iosbd <io232001@gmail.com>
2023-03-08 15:17:23 +0100

    Commit

--- src/it/develhope/encapsulation2/Person.java
+++ src/it/develhope/encapsulation2/Person.java
@@ -13,13 +13,32 @@ public class Person {
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getSurname() {
         return surname;
     }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
     public double getHeight() {
         return height;
     }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
     public int getAge() {
         return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
